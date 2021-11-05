09f7e91e3dd98ff6dffce1ba595c4bb0f2f873cd
Mostaf3wad <3wad747@gmail.com>
2021-11-05 03:07:59 +0200

    challenge2  theExercise4

--- bin/drawing_geometric_shapes/challenge2.dart
+++ bin/drawing_geometric_shapes/challenge2.dart
@@ -8,7 +8,7 @@ import 'dart:io';
 // عند تشغيل البرنامج, يجب أن يطلب من المستخدم إدخال عدد أسطر الشكل الذي سيتم رسمه.
 // إنتبه: يجب أن يدخل المستخدم عدد أكبر من صفر, لأن عدد الأسطر لا يمكن أن يكون صفر أو أقل من صفر.
 void main() {
-  theExercise3();
+  theExercise4();
 }
 
 void theExercise1() {
@@ -52,11 +52,11 @@ void theExercise2() {
 }
 
 void theExercise3() {
-//      *
-//     ***
-//    *****
-//   *******
 //  *********
+//   *******
+//    *****
+//     ***
+//      *
 
   print('Enter the number of Rows: ');
 
@@ -66,12 +66,45 @@ void theExercise3() {
     theExercise3();
   } else {
     var x = numberOfRows + (numberOfRows.isEven ? 3 : 2);
-    var y = numberOfRows - 1;
     for (var i = 1; i <= numberOfRows; i++) {
       print(' ' * i + '*' * x);
-      // print(' ' * i + '*' * (x));
       x -= 2;
-      y--;
+    }
+  }
+}
+
+void theExercise4() {
+// إنتبه: يجب أن يدخل المستخدم عدد مفرد و
+// أكبر من صفر, لأن عدد الأسطر لا يمكن أن يكون مزدوجاً أو قيمته تساوي صفر أو أقل من صفر.
+
+// Note: The user must enter a single number greater than zero
+//, because the number of lines cannot be a double or its value is zero or less than zero.
+
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+
+  print('Enter the number of Rows: ');
+
+  var numberOfRows = int.parse(stdin.readLineSync());
+  if (numberOfRows <= 0) {
+    print('Please Enter a number greater than 0');
+    theExercise3();
+  } else if (numberOfRows.isEven) {
+    print('Please Enter odd number');
+    theExercise3();
+  } else {
+    var x = 2;
+
+    for (var i = 1; i <= numberOfRows * 2; i++) {
+      if (numberOfRows < i) {
+        print('*' * (i - x));
+        x += 2;
+      } else {
+        print('*' * i);
+      }
     }
   }
 }
