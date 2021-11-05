8c38003c4353f65dc643d92a02a80af079588376
Mostaf3wad <3wad747@gmail.com>
2021-11-05 03:23:36 +0200

    challenge2 theExercise5

--- bin/drawing_geometric_shapes/challenge2.dart
+++ bin/drawing_geometric_shapes/challenge2.dart
@@ -8,7 +8,7 @@ import 'dart:io';
 // عند تشغيل البرنامج, يجب أن يطلب من المستخدم إدخال عدد أسطر الشكل الذي سيتم رسمه.
 // إنتبه: يجب أن يدخل المستخدم عدد أكبر من صفر, لأن عدد الأسطر لا يمكن أن يكون صفر أو أقل من صفر.
 void main() {
-  theExercise4();
+  theExercise5();
 }
 
 void theExercise1() {
@@ -80,11 +80,15 @@ void theExercise4() {
 // Note: The user must enter a single number greater than zero
 //, because the number of lines cannot be a double or its value is zero or less than zero.
 
-//  *********
-//   *******
-//    *****
-//     ***
-//      *
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
 
   print('Enter the number of Rows: ');
 
@@ -97,13 +101,53 @@ void theExercise4() {
     theExercise3();
   } else {
     var x = 2;
-
     for (var i = 1; i <= numberOfRows * 2; i++) {
       if (numberOfRows < i) {
         print('*' * (i - x));
         x += 2;
       } else {
         print('*' * i);
+      }
+    }
+  }
+}
+
+void theExercise5() {
+// إنتبه: يجب أن يدخل المستخدم عدد مفرد و
+// أكبر من صفر, لأن عدد الأسطر لا يمكن أن يكون مزدوجاً أو قيمته تساوي صفر أو أقل من صفر.
+
+// Note: The user must enter a single number greater than zero
+//, because the number of lines cannot be a double or its value is zero or less than zero.
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+  print('Enter the number of Rows: ');
+
+  var numberOfRows = int.parse(stdin.readLineSync());
+  if (numberOfRows <= 0) {
+    print('Please Enter a number greater than 0');
+    theExercise3();
+  } else if (numberOfRows.isEven) {
+    print('Please Enter odd number');
+    theExercise3();
+  } else {
+    var x = 2;
+    var y = 1;
+    for (var i = 1; i <= numberOfRows * 2; i++) {
+      if (numberOfRows < i) {
+        print(' ' * y + '*' * (i - x));
+        y++;
+        x += 2;
+      } else {
+        print(' ' * (numberOfRows - i) + '*' * i);
       }
     }
   }
